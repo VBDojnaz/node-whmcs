976b162daccd74f4c1a2686b12dcf760ad088540
Victor Bäck <36706634+VBDojnaz@users.noreply.github.com>
2022-06-08 20:25:22 +0200

    Update products.js

--- modules/products.js
+++ modules/products.js
@@ -16,5 +16,19 @@ class Products {
   addProduct(parameters, callback) {
     return this.whmcsHttpClient.callApi('AddProduct', parameters, callback);
   };
+
+  /**
+   * Adds a product to the system to be available for purchase.
+   * https://developers.whmcs.com/api-reference/addproduct/
+   * @param {Object} parameters Request parameters
+   * @param {String} [parameters.pid] Obtain a specific product id configuration. Can be a list of ids comma separated
+   * @param {String} [parameters.gid] Retrieve products in a specific group id
+   * @param {String} [parameters.module] Retrieve products utilising a specific module
+   * 
+   * @param {Function} callback Optional callback. If not set the method returns a Promise
+   */
+  getProducts(parameters, callback) {
+    return this.whmcsHttpClient.callApi('GetProducts', parameters, callback);
+  };
 }
 module.exports = Products;
